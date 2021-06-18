8bb781b52c36151986128add7a5ed69e20769339
nilesalt <nilesalt@gmail.com>
2021-06-18 14:13:09 +0100

    Dot Files

--- synx.sh
+++ synx.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 R_OPT="-t"
 
-rsync ${R_OPT} ~/.zprofile zprofile
-rsync ${R_OPT} ~/.zshrc zshrc
-rsync ${R_OPT} ~/.tmux.conf tmux.conf
-rsync ${R_OPT} ~/.bash_profile bashProfile
-rsync ${R_OPT} ~/.vimrc vimrc
+rsync ${R_OPT} ~/.zprofile .zprofile
+rsync ${R_OPT} ~/.zshrc .zshrc
+rsync ${R_OPT} ~/.tmux.conf .tmux.conf
+rsync ${R_OPT} ~/.bash_profile .bash_profile
+rsync ${R_OPT} ~/.vimrc .vimrc
 rsync ${R_OPT} ~/.config/fish/config.fish config.fish
