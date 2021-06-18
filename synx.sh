6e37f42a71f587e2c1ccfc79346187923b2b267e
aaneez <aaneez@gmail.com>
2021-06-18 14:18:01 +0100

    Dot Files

--- synx.sh
+++ synx.sh
@@ -6,4 +6,6 @@ rsync ${R_OPT} ~/.zshrc .zshrc
 rsync ${R_OPT} ~/.tmux.conf .tmux.conf
 rsync ${R_OPT} ~/.bash_profile .bash_profile
 rsync ${R_OPT} ~/.vimrc .vimrc
+rsync ${R_OPT} ~/.gitconfig .gitconfig
+rsync ${R_OPT} ~/.gitignore_global .gitignore_global
 rsync ${R_OPT} ~/.config/fish/config.fish config.fish
