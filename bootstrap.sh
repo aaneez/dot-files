840a1bfb7237c97dbdccd417daf10c408c9603d7
aaneez <aaneez@gmail.com>
2021-06-19 07:18:04 +0100

    Dot Files

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,9 +13,6 @@ else
 fi
 
 install_fonts() {
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
+	git clone https://github.com/powerline/fonts.git --depth=1
+	cd fonts; ./install.sh; cd ..; rm -rf fonts
 }
